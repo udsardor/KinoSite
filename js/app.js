b767075f3d3054d8f12c4fc17a76fa64eeaa2c0c
udsardor <udsardor@gmail.com>
2022-12-03 14:28:27 +0500

    Template Has Finished

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const API_KEY = "8c8e1a50-6322-4135-8875-5d40a5420d86";
+const API_URL_POPULAR =
+  "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1";
+const API_URL_SEARCH =
+  "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=";
+
